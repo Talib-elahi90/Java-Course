e0578aa5f68fa411b22c624ce04bd199db641605
Talib Ilahi <talibelahi90@gmail.com>
2025-05-05 20:33:08 +0500

    Modify Arrays.java file.

--- java-tutorial/Arrays.java
+++ java-tutorial/Arrays.java
@@ -72,5 +72,47 @@ public class Arrays {
             System.out.println();
         }
 
+        // This program generates the following output:
+        // 0
+        // 1 2
+        // 3 4 5
+        // 6 7 8 9
+
+        // Three-dimensional array.
+
+        // int threeD[][][] = new int[3][4][5];
+        // int i, j, k;
+        // for (i = 0; i < 3; i++)
+        // for (j = 0; j < 4; j++)
+        // for (k = 0; k < 5; k++)
+        // threeD[i][j][k] = i * j * k;
+        // for (i = 0; i < 3; i++) {
+        // for (j = 0; j < 4; j++) {
+        // for (k = 0; k < 5; k++)
+        // System.out.print(threeD[i][j][k] + " ");
+        // System.out.println();
+        // }
+        // System.out.println();
+        // }
+
+        /*
+         * This program generates the following output:
+         * 0 0 0 0 0
+         * 0 0 0 0 0
+         * 0 0 0 0 0
+         * 0 0 0 0 0
+         * 
+         * 0 0 0 0 0
+         * 0 1 2 3 4
+         * 0 2 4 6 8
+         * 0 3 6 9 12
+         * 
+         * 0 0 0 0 0
+         * 0 2 4 6 8
+         * 0 4 8 12 16
+         * 0 6 12 18 24
+         */
+
+         
     }
 }
---
+++ java-tutorial/Enums.java
@@ -0,0 +1,39 @@
+// Enums allows us to make constants in java
+public class Enums {
+    // This is our constants
+    enum Gender  {
+        MALE,
+        FEMALE
+    }
+
+    enum ShoeSize  {
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        ELEVEN,
+        TWELVE
+    }
+
+    
+    public static void main(String[] args) {
+        // enum
+        
+        System.out.println(Gender.MALE);
+        System.out.println(Gender.FEMALE);
+
+        // OR 
+
+        Gender male = Gender.MALE;
+        Gender female = Gender.FEMALE;
+
+        System.out.println(male);
+        System.out.println(female);
+        // Output
+        // MALE
+        // FEMALE
+        
+        
+    }
+}
