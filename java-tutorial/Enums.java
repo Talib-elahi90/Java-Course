22644d458b04baae7ee0d086fdbc7fe582a6540c
Talib Ilahi <talibelahi90@gmail.com>
2025-05-06 15:01:37 +0500

    Add some code in this file.

--- java-tutorial/Enums.java
+++ java-tutorial/Enums.java
@@ -1,12 +1,14 @@
+import java.util.*;
+
 // Enums allows us to make constants in java
 public class Enums {
     // This is our constants
-    enum Gender  {
+    enum Gender {
         MALE,
         FEMALE
     }
 
-    enum ShoeSize  {
+    enum ShoeSize {
         SIX,
         SEVEN,
         EIGHT,
@@ -16,24 +18,32 @@ public class Enums {
         TWELVE
     }
 
-    
     public static void main(String[] args) {
         // enum
-        
+
         System.out.println(Gender.MALE);
         System.out.println(Gender.FEMALE);
 
-        // OR 
+        // OR
 
         Gender male = Gender.MALE;
         Gender female = Gender.FEMALE;
+
+        // Convert an enum to String
+        System.out.println("Enum to String " + Gender.valueOf("FEMALE"));
+
+        // Loop 
+        System.out.println("Shoe Size Loop ");
+        for(ShoeSize shoe: ShoeSize.values()){
+            System.out.println(shoe);
+        }
+        System.out.println();
 
         System.out.println(male);
         System.out.println(female);
         // Output
         // MALE
         // FEMALE
-        
-        
+
     }
 }
