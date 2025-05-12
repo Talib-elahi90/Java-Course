dc188de72c8d665d29fcc7618f1ac7eb0548ea19
Talib Ilahi <talibelahi90@gmail.com>
2025-05-12 09:26:24 +0500

    add some code into this class.

--- java-tutorial/Userinput.java
+++ java-tutorial/Userinput.java
@@ -1,6 +1,11 @@
-public class ScannerClass {
+import java.util.Scanner;
+
+public class Userinput {
 
     public static void main(String[] args) {
-        
+        System.out.println("What's your name?");
+        Scanner scanner = new Scanner(System.in);
+        String result = scanner.nextLine();
+        System.out.println("Hay " + result);
     }
 }
