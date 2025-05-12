bbc4f9981edcdb036e800f93fc0b628686ceb312
Talib Ilahi <talibelahi90@gmail.com>
2025-05-12 09:36:00 +0500

    add some line of code, using .nextInt().

--- java-tutorial/Userinput.java
+++ java-tutorial/Userinput.java
@@ -4,8 +4,13 @@ public class Userinput {
 
     public static void main(String[] args) {
         System.out.println("What's your name?");
-        Scanner scanner = new Scanner(System.in);
-        String result = scanner.nextLine();
-        System.out.println("Hay " + result);
+        Scanner scanner = new Scanner(System.in); // Create Scanner class object
+        String result = scanner.nextLine(); // store String into a result variable .nextLine return string
+        System.out.println("Hay " + result); // print result
+
+        System.out.print("Your age? ");
+        int age = scanner.nextInt(); // .nextInt return an Integer
+        System.out.println(age);
     }
+
 }
