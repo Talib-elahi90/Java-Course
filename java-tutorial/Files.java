0d5f0bd5506c9bc5b9eebc71f9e9b3857913fc85
Talib Ilahi <talibelahi90@gmail.com>
2025-05-16 08:49:59 +0500

    Add code for write and read file.

--- java-tutorial/Files.java
+++ java-tutorial/Files.java
@@ -1,21 +1,70 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class Files {
 
     public static void main(String[] args) {
+        // File create and delete.
+        /*
+         * try {
+         * File file = new File("foo.txt"); // give a path to create a file.
+         * if (!file.exists()) {
+         * // Add a new file
+         * file.createNewFile(); // must include try catch and throw exception.
+         * // File created.
+         * }
+         * // If you want to delete a file
+         * if (file.exists()){
+         * file.delete();
+         * }
+         * } catch (Exception e) {
+         * System.out.println(e.getMessage());
+         * }
+         */
+
+        // Write data in file
+        File file = createFile("java-tutorial/data.txt");
+        // writeFile(file, false);
+        readFile(file);
+
+    }
+
+    private static void readFile(File file) {
         try {
-            File file = new File("foo.txt"); // give a path to create a file.
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    private static void writeFile(File file, boolean append) {
+        try {
+            FileWriter fileWriter = new FileWriter(file, append); // If you want to append data used true in second
+                                                                  // args.
+            PrintWriter write = new PrintWriter(fileWriter);
+            write.println("Software Engineer.");
+            write.flush();
+            write.close();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static File createFile(String path) {
+        try {
+            File file = new File(path);
             if (!file.exists()) {
-                // Add a new file
-                file.createNewFile(); // must include try catch and throw exception.
-                // File created.
+                file.createNewFile();
             }
-            // If you want to delete a file
-            if (file.exists()){
-                file.delete();
-            }
+            return file;
         } catch (Exception e) {
             System.out.println(e.getMessage());
+            throw new IllegalStateException(e);
         }
 
     }
