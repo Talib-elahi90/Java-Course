0208f418e291627cf832bed6b8b21139bc9b6d9c
Talib Ilahi <talibelahi90@gmail.com>
2025-05-17 09:31:50 +0500

    Create a separate class Cat.java

---
+++ OOP/Cat.java
@@ -0,0 +1,17 @@
+package OOP;
+
+public class Cat {
+    // If you want to access it in a main class you must add get and set methods.
+    private String name; // field
+
+    void meow() { // method or behavoir
+        System.out.println(name + ": meow...");
+    }
+
+    // This setter allows us to set correspondence value.
+    public void setName(String name) {
+        // this keyword is used to access current instance object of the class.
+        this.name = name;
+    }
+
+}
