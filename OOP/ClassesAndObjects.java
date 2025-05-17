2182a378cf7d4489b8f7997d1e0a6b462bee820e
Talib Ilahi <talibelahi90@gmail.com>
2025-05-17 09:33:51 +0500

    Edit code and set name of Cat class.

--- OOP/ClassesAndObjects.java
+++ OOP/ClassesAndObjects.java
@@ -3,15 +3,24 @@ package OOP;
 // A class is a blueprint for creating objects, and an object is an instance of a class.
 public class ClassesAndObjects {
     public static void main(String[] args) {
-        Cat cat = new Cat();
-        cat.meow();
+        // Accessing directly to the property of the class is bad.
+        // cat.name = "Hoho"; // like this is actually bad.
+        Cat kitty = new Cat();
+        kitty.setName("Kitty");
+        kitty.meow();
+        
+        Cat sunCat = new Cat();
+        sunCat.setName("Sun");
+        sunCat.meow();
     }
 
+    /*
+    This is not god create a new class different class in the same package.
     static class Cat {
-        String name;
+    
+    private String name; // field
 
-        void meow() {
-            System.out.println(name + ": meow...");
-        }
-    }
+    void meow() { // method or behavoir
+        System.out.println(name + ": meow...");
+    } */
 }
