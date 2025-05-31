656c1b6240bd35342bd92584dd374d9ddbbd1e2e
Talib Ilahi <talibelahi90@gmail.com>
2025-05-31 17:35:29 +0500

    Add a new class AbstrackKeyword.java

---
+++ OOP/AbstractKeyword.java
@@ -0,0 +1,79 @@
+package OOP;
+
+import java.util.Objects;
+
+public class AbstractKeyword {
+    public static void main(String[] args) {
+        Snake snake = new Snake("snicky");
+        Animals parrot = new Bird("parrot");
+
+        snake.makeSound();
+        parrot.makeSound();
+    }
+}
+
+// This class cannot be instanciated, it use with an inheritance.
+abstract class Animals {
+    private String name;
+
+    public Animals(String name) {
+        this.name = name;
+    }
+
+    public abstract void makeSound();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Animals [name=" + name + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Animals other = (Animals) obj;
+        return Objects.equals(name, other.name);
+    }
+
+}
+
+class Snake extends Animals {
+
+    public Snake(String name) {
+        super(name);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("yss ysse");
+    }
+
+}
+
+class Bird extends Animals {
+
+    public Bird(String name) {
+        super(name);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("auaaan");
+    }
+
+}
