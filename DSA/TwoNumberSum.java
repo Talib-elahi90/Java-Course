e0f68b23511bca468591b596ab4b9aa97ea28a20
Talib Ilahi <talibelahi90@gmail.com>
2025-06-08 12:30:16 +0500

    Add some changes in the code.

--- DSA/TwoNumberSum.java
+++ DSA/TwoNumberSum.java
@@ -1,10 +1,12 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 class TwoNumberSum {
     public static void main(String[] args) {
         int[] array = { 2, 5, 6, 8, 9, -1, 7, -3 };
-        int target = 15;
+        int target = 20;
 
         System.out.println(Arrays.toString(twoNumberSum(array, target)));
     }
@@ -22,34 +24,35 @@ class TwoNumberSum {
     // return null;
     // }
 
-    private static int[] twoNumberSum(int[] array, int target) {
-        int l = 0;
-        int r = array.length - 1;
-        Arrays.sort(array);
-        while (l < r) {
-            int currentSum = array[l] + array[r];
-            if (currentSum == target) {
-                return new int[] {array[l], array[r]};
-            } else if (currentSum < target){
-                l++;
-            } else r++;
-        }
-        return new int[]{};
-    }
-
-    // Time O(N), Space O(N)
+    // O(NlogN) | Space O(1)
     // private static int[] twoNumberSum(int[] array, int target) {
-    // var nums = new HashSet<>();
-    // for(int i = 0; i < array.length; i++) {
-    // int match = target - array[i];
-    // if(nums.contains(match)){
-    // return new int[] {match, array[i]};
-    // }
-    // else {
-    // nums.add(array[i]);
-    // }
+    // int l = 0;
+    // int r = array.length - 1;
+    // Arrays.sort(array);
+    // while (l < r) {
+    // int currentSum = array[l] + array[r];
+    // if (currentSum == target) {
+    // return new int[] { array[l], array[r] };
+    // } else if (currentSum < target) {
+    // l++;
+    // } else
+    // r++;
     // }
     // return new int[] {};
     // }
+
+    // Time O(N), Space O(N)
+    private static int[] twoNumberSum(int[] array, int target) {
+        List nums = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            int match = target - array[i];
+            if (nums.contains(match)) {
+                return new int[] { match, array[i] };
+            } else {
+                nums.add(array[i]);
+            }
+        }
+        return new int[] {};
+    }
 
 }
