716cc2bf9e78240bb07582a6e5a3c19bc856cef0
Talib Ilahi <talibelahi90@gmail.com>
2025-06-16 17:46:31 +0500

    Solve a new question Validate Sub sequence.

---
+++ DSA/ValidateSubSequence.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class ValidateSubSequence {
+    public static void main(String[] args) {
+        // {8, 6, 7, 10, -1, 3, -10, 9, 11}
+        // {7, -1, 8, 9}
+        int[] array = { 8, 6, 7, -1, 3, -10, 9, 11 };
+        int[] subArray = { 7, -1, -10, 9 };
+
+        System.out.println(subSequence(array, subArray));
+
+    }
+
+    private static boolean subSequence(int[] array, int[] subArray) {
+        int arrIdx = 0;
+        int seqIdx = 0;
+        while (arrIdx < array.length && seqIdx < subArray.length) {
+            if (array[arrIdx] == subArray[seqIdx]) {
+                seqIdx++;
+            }
+            arrIdx++;
+        }
+        return seqIdx == subArray.length;
+    }
+
+    private static boolean subSequence1(int[] array, int[] subArray) {
+        int seqIdx = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (seqIdx < subArray.length) {
+                return true;
+            }
+            if (array[i] == subArray[seqIdx]) {
+                seqIdx++;
+            }
+        }
+        return seqIdx == subArray.length;
+    }
+}
