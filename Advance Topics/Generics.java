3035a01fd16f3582d20d2341e133561e7180a6ff
Talib Ilahi <talibelahi90@gmail.com>
2025-06-24 06:22:00 +0500

    Add new folder in this course.

---
+++ Advance Topics/Generics.java
@@ -0,0 +1,57 @@
+public class Generics {
+    public static void main(String[] args) {
+        Box<Phone> box = new Box<>();
+        box.set(new Phone("One Plus."));
+        System.out.println(box.get());
+
+    }
+}
+
+// Create Generic class, if you want to replace T with any alphabet, you could.
+class Box<T> {
+    private T t;
+
+    public void set(T t) {
+        this.t = t;
+    }
+
+    public T get() {
+        return t;
+    }
+}
+
+class Phone {
+    private final String brand;
+
+    public Phone(String brand) {
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone [brand=" + brand + "]";
+    }
+
+}
+
+class Letter {
+    private final String letter;
+
+    public Letter(String letter) {
+        this.letter = letter;
+    }
+
+    public String getLetter() {
+        return letter;
+    }
+
+    @Override
+    public String toString() {
+        return "Letter [letter=" + letter + "]";
+    }
+
+}
