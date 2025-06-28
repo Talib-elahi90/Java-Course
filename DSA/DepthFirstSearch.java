e08e2a5b6740d5de85a96e1371c26cd7ac4bce24
Talib Ilahi <talibelahi90@gmail.com>
2025-06-28 20:59:34 +0500

    DepthFirstSearch algorithm

--- DSA/DepthFirstSearch.java
+++ DSA/DepthFirstSearch.java
@@ -1,5 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class DepthFirstSearch {
     public static void main(String[] args) {
-        
+        Node root = new Node("A");
+        root.addChild("B").addChild("C");
+        root.child.get(0).addChild("D").addChild("E");
+        root.child.get(1).addChild("F");
+
+        List<String> result = new ArrayList<>();
+        root.depthFirstSearch(result);
+
+        System.out.println("DFS Result: " + result);
+    }
+}
+
+class Node {
+    String name;
+    List<Node> child;
+
+    public Node(String name) {
+        this.name = name;
+        this.child = new ArrayList<>();
+    }
+
+    public Node addChild(String name) {
+        Node child = new Node(name);
+        this.child.add(child);
+        return this; // allows chaining
+    }
+
+    public List<String> depthFirstSearch(List<String> array) {
+        array.add(this.name);
+        for (Node child : child) {
+            child.depthFirstSearch(array);
+        }
+        return array;
     }
 }
